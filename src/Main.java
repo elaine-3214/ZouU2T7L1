8be88772904a70c01a4628468dd8252ffb7ca40d
1E10 Laptop <sample@bths.edu>
2022-10-11 12:32:51 -0400

    draft

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main (String[] args) {
+        String str1 = "Hello!";
+        String str2 = "Hello!";
+        String str3 = new String("Hello!");
+        String str4 = new String("Hello!");
+
+        System.out.println(str1 == str2);
+        System.out.println(str1 == str3);
+        System.out.println(str1 == str4);
+        System.out.println(str3 == str4);
+
+    }
 }
